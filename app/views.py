2d97567747aa481758d65c243d5d2e872b67d698
Alex Leschenko <a_leshenko@tut.by>
2016-07-07 10:58:21 +0300

    done up%down in list

--- app/views.py
+++ app/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render, redirect
 from models import *
 from forms import *
+from django.http import HttpResponse
 # Create your views here.
 def main(request):
     if request.GET.items():
         action = request.GET.get('action')
         id = request.GET.get('id')
         if action == 'dell':
-            Todo.objects.filter(id=int(id)).delete()
+            Todo.objects.filter(id=id).delete()
             return redirect('main')
+        elif action == 'upp':
+            if id == 1:
+                return redirect('main')
+            else:
+                taskup = Todo.objects.filter(id=id).get()
+                taskdown = Todo.objects.filter(id=int(id) - 1).get()
+                Todo.objects.filter(id=taskup.id).update(task=taskdown.task, done=taskdown.done)
+                Todo.objects.filter(id=taskdown.id).update(task=taskup.task, done=taskup.done)
+                return redirect('main')
+        elif action == 'down':
+            data = Todo.objects.filter().values('id')
+            max_id = 0
+            for i in data:
+                if i > max_id:
+                    max_id = i['id']
+            if int(id) == max_id:
+                return redirect('main')
+            else:
+                taskdown = Todo.objects.filter(id=id).get()
+                taskup = Todo.objects.filter(id=int(id) + 1).get()
+                Todo.objects.filter(id=taskup.id).update(task=taskdown.task, done=taskdown.done)
+                Todo.objects.filter(id=taskdown.id).update(task=taskup.task, done=taskup.done)
+                return redirect('main')
     else:
         data = Todo.objects.filter()
         context = {'all_data':data}
