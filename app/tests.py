e42815871c7cf4c455d96a2cecfd0affc71c92a9
Alex Leschenko <a_leshenko@tut.by>
2016-07-09 17:22:39 +0300

    test_ok 09.07

---
+++ app/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from models import *
+# Create your tests here.
+class ProjectTest(TestCase):
+
+    def test_ok_create_task(self):
+
+        data = {'task':'my_task', 'done':False}
+        self.client.post('/add/', data)
+        q_task = Todo.objects.filter()
+        self.assertEquals(q_task.count(), 1)
+        task = q_task.get()
+        self.assertEquals(task.task, data['task'])
+        self.assertEquals(task.done, data['done'])
+
+    def test_ok_delete_task(self):
+
+        Todo.objects.create(task='my_task', done=True)
+        task = Todo.objects.filter().last()
+        self.client.get('', {'action':'dell', 'id':task.id})
+        q_task = Todo.objects.filter()
+        self.assertEquals(q_task.count(), 0)
+
+    def test_ok_update_task(self):
+        Todo.objects.create(task='my_task', done=True)
+        task = Todo.objects.filter().last()
+        self.client.get('/update/', { 'id': task.id})
+        data = {'task':'new_task', 'done':False}
+        self.client.post('/update/', data)
+        task = Todo.objects.filter().get()
+        self.assertEquals(task.task, data['task'])
+        self.assertEquals(task.done, data['done'])
